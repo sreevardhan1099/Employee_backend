961f0484548188b6e0b26c63a19c9d491cebc7ca
vardhan <sreevardhan1099@gmail.com>
2023-01-10 10:28:47 -0500

    just comiting the repo to git

---
+++ src/main/java/com/firstproject/learningproject/LearningprojectApplication.java
@@ -0,0 +1,13 @@
+package com.firstproject.learningproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningprojectApplication.class, args);
+	}
+
+}
